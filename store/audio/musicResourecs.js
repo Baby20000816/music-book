30d038904d524aea8145dd7685489c3462d3d525
unknown <1273163614@qq.com>
2020-11-12 14:14:09 +0800

    音乐模块

---
+++ store/audio/musicResourecs.js
@@ -0,0 +1,30 @@
+export default {
+	musicResourecs: [{
+			id: 10001,
+			name: 'SummerBoy',
+			src: 'http://music.163.com/song/media/outer/url?id=2664387.mp3',
+			singer: {
+				name: 'SummerBoy',
+				synopsis: '阴阳怪气组合'
+			}
+		},
+		{
+			id: 10002,
+			name: '抠抠',
+			src: '/static/mp3/koukou.mp3',
+			singer: {
+				name: '老番茄',
+				synopsis: '老番茄，b站up主'
+			}
+		},
+		{
+			id: 10003,
+			name: '自夸小队',
+			src: '/static/mp3/zikua.mp3',
+			singer: {
+				name: '阴阳怪气',
+				synopsis: '阴阳怪气组合'
+			}
+		}
+	]
+}
